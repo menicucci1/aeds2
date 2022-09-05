95adcf12aa9c4b70c160a280cddca0e01d83a1ba
Enrico <emgomes@sga.pucminas.br>
2022-09-05 17:12:33 -0300

    atualizando arquivos

--- labs/lab02/combinador.c
+++ labs/lab02/combinador.c
@@ -5,22 +5,31 @@ int main(){
     int length, length1, length2;
     char string1[100], string2[100];
 
-    scanf("%s %s", string1, string2);
-    length1 = strlen(string1);
-    length2 = strlen(string2);
-    if (length1 >= length2){
-        length = length1;
-    }else{
-        length = length2;
-    }
-    for (int i = 0; i < length; i++){
-        if(i < length1){
-            printf("%c", string1[i]);
+    while (scanf("%s%s", string1, string2) == 2)
+    {
+        length1 = strlen(string1);
+        length2 = strlen(string2);
+        if (length1 >= length2)
+        {
+            length = length1;
         }
-        if(i < length2){
-            printf("%c", string2[i]);
+        else
+        {
+            length = length2;
         }
+        for (int i = 0; i < length; i++)
+        {
+            if (i < length1)
+            {
+                printf("%c", string1[i]);
+            }
+            if (i < length2)
+            {
+                printf("%c", string2[i]);
+            }
+        }
+        printf("\n");
     }
-    printf("\n");
+
     return 0;
 }
