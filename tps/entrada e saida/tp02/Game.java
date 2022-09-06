a09fd606a85dbba5e7b4b1b08caf8a9668f6316c
Enrico <emgomes@sga.pucminas.br>
2022-09-06 18:12:50 -0300

    tp02

--- tps/entrada e saida/tp02/Game.java
+++ tps/entrada e saida/tp02/Game.java
@@ -1,6 +1,9 @@
+import java.time.LocalDate;
+
 public class Game {
     private int app_id, age, dlcs, avg_pt;
-    private String name, release_date, owners, developers;
+    private LocalDate release_date;
+    private String name, owners, developers;
     private boolean windows, linux, mac;
     private float price, upvotes;
     private String[] languages, genres; 
@@ -21,7 +24,6 @@ public class Game {
         this.upvotes = 0.0;
         this.languages = "";
         this.genres = "";
-        
     }
 
     public Game(int app_id, int age, int avg_pt, int dlcs, String name, String release_date, String owners, String website, String developers, float price, String[] languages, boolean windows, boolean mac, boolean linux, float upvotes, String[] genres){
@@ -42,124 +44,7 @@ public class Game {
         this.genres = genres;
     }
 
-    public int getAppId(){
-        return this.app_id;
-    }
-
-    public void setAppId(int app_id){
-        this.app_id = app_id;
-    }
-
-    public int getAge(){
-        return this.age;
-    }
-
-    public void setAge(int age){
-        this.age = age;
-    }
-
-    public int getAvgPt(){
-        return this.avg_pt;
-    }
-
-    public void setAvgPt(int avg_pt){
-        this.avg_pt = avg_pt;
-    }
-
-    public int getDlcs(){
-        return this.dlcs;
-    }
-
-    public void setDlcs(int dlcs){
-        this.dlcs = dlcs;
-    }
-
-    public String getName(){
-        return this.name;
-    }
-
-    public void setName(String name){
-        this.name = name;
-    }
-
-    public String getReleaseDate(){
-        return this.release_date;
-    }
-
-    public void setReleaseDate(String release_date){
-        this.release_date = release_date;
-    }
-
-    public String getOwners(){
-        return this.owners;
-    }
-
-    public void setOwners(String owners){
-        this.owners = owners;
-    }
-
-    public String getDevelopers(){
-        return this.developers;
-    }
-
-    public void setDevelopers(String developers){
-        this.developers = developers;
-    }
-
-    public boolean getWindows(){
-        return this.windows;
-    }
-
-    public void setWindows(boolean windows){
-        this.windows = windows;
-    }
-
-    public boolean getLinux(){
-        return this.linux;
-    }
-
-    public void setLinux(boolean linux){
-        this.linux = linux;
-    }
-    public boolean getMac(){
-        return this.mac;
-    }
-
-    public void setMac(boolean mac){
-        this.mac = mac;
-    }
     
-    public float getPrice(){
-        return this.price;
-    }
-
-    public void setPrice(float price){
-        this.price = price;
-    }
-
-    public float getUpvotes(){
-        return this.upvotes;
-    }
-
-    public void setUpvotes(float upvotes){
-        this.upvotes = upvotes;
-    }
-
-    public String[] getLanguages(){
-        return this.languages;
-    }
-    
-    public void setLanguages(String[] languages){
-        this.languages = languages;
-    }
-
-    public String[] getGenres(){
-        return this.genres;
-    }
-    
-    public void setGenres(String[] genres){
-        this.genres = genres;
-    }
     
     
 
